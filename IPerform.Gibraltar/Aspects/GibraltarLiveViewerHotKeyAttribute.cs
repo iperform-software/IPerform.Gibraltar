a09980dfb6bc107d67f660cb689fa73b9dd8537e
dsmit <ds@iperform.nl>
2012-02-14 16:42:51 +0100

    Importing.

---
+++ IPerform.Gibraltar/Aspects/GibraltarLiveViewerHotKeyAttribute.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PostSharp.Aspects;
+using PostSharp.Aspects.Advices;
+using PostSharp.Extensibility;
+using PostSharp.Reflection;
+using System.Windows.Forms;
+using Gibraltar.Agent;
+
+namespace IPerform.Gibraltar.Aspects
+{
+    [Serializable]
+    [MulticastAttributeUsage(MulticastTargets.Class,
+                              Inheritance = MulticastInheritance.Strict)]
+    public class GibraltarLiveViewerHotKeyAttribute : InstanceLevelAspect
+    {
+        public readonly Keys keys;
+
+        public GibraltarLiveViewerHotKeyAttribute(Keys keys)
+        {
+            this.keys = keys;
+        }
+
+        public GibraltarLiveViewerHotKeyAttribute()
+        {
+            this.keys = Keys.OemQuestion;
+        }
+
+        [IntroduceMember(Visibility = Visibility.Family, IsVirtual = true,
+                    OverrideAction = MemberOverrideAction.OverrideOrFail)]
+        public bool ProcessCmdKey(ref System.Windows.Forms.Message msg, Keys keyData)
+        {
+            if (keyData == keys)
+            {
+                Log.ShowLiveViewer();
+
+                return true;
+            }
+
+            return false;
+        //    return base.ProcessCmdKey(ref msg, keyData); // FIXME :(
+        }
+    }
+}
---
+++ IPerform.Gibraltar/Aspects/UseGibraltarAttribute.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PostSharp.Extensibility;
+using PostSharp.Aspects;
+using Gibraltar.Agent;
+
+namespace IPerform.Gibraltar.Aspects
+{
+    [Serializable]
+    public class UseGibraltarAttribute : OnMethodBoundaryAspect
+    {
+        readonly string companyName;
+        readonly string serviceName;
+        readonly string privacyPolicyUrl;
+        readonly bool autoSendConsent;
+        readonly bool consentDefault;
+        readonly int promptUserOnStartupLimit;
+
+        public UseGibraltarAttribute(string companyName, string serviceName, string privacyPolicyUrl, bool autoSendConsent, bool consentDefault, int promptUserOnStartupLimit)
+        {
+            this.companyName = companyName;
+            this.serviceName = serviceName;
+            this.privacyPolicyUrl = privacyPolicyUrl;
+            this.autoSendConsent = autoSendConsent;
+            this.consentDefault = consentDefault;
+            this.promptUserOnStartupLimit = promptUserOnStartupLimit;
+        }
+
+        public override void OnEntry(MethodExecutionArgs args)
+        {
+            System.Windows.Forms.Application.EnableVisualStyles();
+            System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(false);
+
+            Log.Initializing += new Log.InitializingEventHandler(Log_Initializing);
+            Log.StartSession("Application starting.");
+
+            Log.DisplayStartupConsentDialog();
+        }
+
+        void Log_Initializing(object sender, LogInitializingEventArgs e)
+        {
+            e.Configuration.AutoSendConsent.CompanyName = companyName;
+            e.Configuration.AutoSendConsent.ServiceName = serviceName;
+            e.Configuration.AutoSendConsent.PrivacyPolicyUrl = privacyPolicyUrl;
+            e.Configuration.AutoSendConsent.Enabled = autoSendConsent;
+            e.Configuration.AutoSendConsent.ConsentDefault = consentDefault;
+            e.Configuration.AutoSendConsent.PromptUserOnStartupLimit = promptUserOnStartupLimit;
+        }
+
+        public override void OnException(MethodExecutionArgs args)
+        {
+            Log.Error(args.Exception, "exception", "", "", null);
+        }
+
+        public override void OnExit(MethodExecutionArgs args)
+        {
+            Log.EndSession("Application stopping.");
+        }
+    }
+}
